43ae5dbcd623e76ff15a167851f46328b2900b8d
pengfan <2898889396@qq.com>
2021-12-07 18:27:40 +0800

    材质

---
+++ 材质/cube-material/cube-material/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  cube-material
+//
+//  Created by pengfan on 2021/12/7.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ 材质/cube-material/cube-material/ViewController.h
@@ -0,0 +1,21 @@
+//
+//  ViewController.h
+//  Matrix-Transform
+//
+//  Created by pengfan on 2021/11/26.
+//
+
+#import <UIKit/UIKit.h>
+#import <GLKit/GLKit.h>
+typedef struct Material {
+    float[3] ambient;
+    float[3] diffuse;
+    float[3] 
+};
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ViewController : GLKViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
